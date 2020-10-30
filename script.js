b17ba56fa09e80ff6a10c6e35ab92f70914d0c74
gunkarlsson <hi@gunkarlsson.com>
2020-10-30 09:44:36 +0100

    updated all

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-var character = document.querySelector('#character');
-var cactus = document.querySelector('#cactus');
+var character = document.querySelector("#character");
+var skull = document.querySelector("#skull");
+var score = 0;
+var highScore = 0;
+
 // var html = document.querySelector('html');
 
-function jump(){
+function jump() {
   if (character.classList != "animate") {
     character.classList.add("animate");
   }
-    setTimeout(()=>{
-      character.classList.remove("animate");
-    },1000);
+  setTimeout(() => {
+    character.classList.remove("animate");
+  }, 1000);
 }
-var gameOver = setInterval(()=>{
-  var dinoTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+var gameOver = setInterval(() => {
+  var ghostTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  var skullLeft = parseInt(
+    window.getComputedStyle(skull).getPropertyValue("left")
+  );
 
-  if(cactusLeft<85 && cactusLeft>0 && dinoTop>=110){
-    cactus.style.animation = "none";
-    cactus.style.display = "none";
-    document.querySelector('.over').style.display = "block";
+  if (skullLeft < 85 && skullLeft > 0 && ghostTop >= 110) {
+    skull.style.animation = "none";
+    skull.style.display = "none";
+    document.querySelector(".over").style.display = "block";
+    //* Kollar om nuvarande score är högre än highScore, isf ersätts highScore med nya siffran
+    if (score > highScore && Math.floor(score / 100) > 0) {
+      highScore = score;
+      document.getElementById("highScoreSpan").innerHTML = Math.floor(
+        highScore / 100
+      );
+    }
+    //* Återställer poängen till 0
+    score = 0;
+    clearInterval(gameOver);
+  } else {
+    score++;
+    //* Skriver ut poäng och rekordpoäng på motsvarande html-element i spelet
+    document.getElementById("highScoreSpan").innerHTML = Math.floor(
+      highScore / 100
+    );
+    document.getElementById("scoreSpan").innerHTML = Math.floor(score / 100);
   }
-},10);
+}, 10);
+
 function reload() {
-    location.reload();
+  location.reload();
 }
