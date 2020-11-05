e7b33a7ada118ffa5679e90417a6c7e6db9c75f7
cr4y0n <filipa.spajic@student.kyh.se>
2020-11-05 23:21:57 +0100

    Added key input, persistent high score, renamed variables

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-var character = document.querySelector("#character");
-var skull = document.querySelector("#skull");
-var score = 0;
+var player = document.querySelector("#player");
+var obstacle = document.querySelector("#obstacle");
+var score = 0; 
 var highScore = 0;
+var activeKeys = {};
 
 // var html = document.querySelector('html');
 
+document.addEventListener("keydown", function(keyboardEvent) { //läser av tangentnedtryckningar
+  activeKeys[keyboardEvent.code] = true; //"aktiverar" aktuell nedtryckt tangents id
+  if(activeKeys["Space"]) { //kontrollerar om nedtryckt tangent motsvarar mellanslag
+    jump();
+  }
+  if(activeKeys["Enter"] || activeKeys["NumpadEnter"]) { //kontrollerar om nedtryckt tangent motsvarar enter
+    reload();
+  }
+}); 
+document.addEventListener("keyup", function(keyboardEvent) { //läser av tangentsläpp
+  activeKeys[keyboardEvent.code] = false; //"inaktiverar" aktuell nedtryckt tangents id
+});
+
 function jump() {
-  if (character.classList != "animate") {
-    character.classList.add("animate");
+  if(player.classList != "animate") {
+    player.classList.add("animate");
   }
   setTimeout(() => {
-    character.classList.remove("animate");
+    player.classList.remove("animate");
   }, 1000);
 }
 var gameOver = setInterval(() => {
-  var ghostTop = parseInt(
-    window.getComputedStyle(character).getPropertyValue("top")
-  );
-  var skullLeft = parseInt(
-    window.getComputedStyle(skull).getPropertyValue("left")
-  );
+  var ghostTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+  var obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
 
-  if (skullLeft < 90 && skullLeft > 0 && ghostTop >= 110) {
-    skull.style.animation = "none";
-    skull.style.display = "none";
-    document.querySelector(".over").style.display = "block";
+  if(obstacleLeft < 90 && obstacleLeft > 0 && ghostTop >= 110) {
+    obstacle.style.animation = "none";
+    obstacle.style.display = "none";
+    document.querySelector(".message").style.display = "block";
     //* Kollar om nuvarande score är högre än highScore, isf ersätts highScore med nya siffran
-    if (score > highScore && Math.floor(score / 100) > 0) {
+    if(score > highScore && Math.floor(score / 100) > 0) {
       highScore = score;
-      document.getElementById("highScoreSpan").innerHTML = Math.floor(
-        highScore / 100
-      );
+      document.getElementById("highScoreBoard").innerHTML = Math.floor(highScore / 100);
+    }
+    if(typeof(Storage) !== "undefined") { //kontrollerar om webläsaren stödjer lokal lagring
+      localStorage.setItem("highScore", highScore); //sparar värdet från variabeln i attributet "highScore"
     }
     //* Återställer poängen till 0
     score = 0;
@@ -38,10 +49,8 @@ var gameOver = setInterval(() => {
   } else {
     score++;
     //* Skriver ut poäng och rekordpoäng på motsvarande html-element i spelet
-    document.getElementById("highScoreSpan").innerHTML = Math.floor(
-      highScore / 100
-    );
-    document.getElementById("scoreSpan").innerHTML = Math.floor(score / 100);
+    document.getElementById("highScoreBoard").innerHTML = Math.floor(localStorage.getItem("highScore") / 100);
+    document.getElementById("scoreBoard").innerHTML = Math.floor(score / 100);
   }
 }, 10);
 
